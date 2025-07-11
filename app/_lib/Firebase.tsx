61121095bca8be98f601f4996e21fcce7143b82f
Austin Liao <131895709+Austin-Almighty@users.noreply.github.com>
2025-07-11 15:59:40 -0700

    Refactor code structure for improved readability and maintainability

--- app/_lib/Firebase.tsx
+++ app/_lib/Firebase.tsx
@@ -6,14 +6,14 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDEA3-cLbNReVaDWANo3-HaB2-EXI8tL-M",
-  authDomain: "typing-app-566de.firebaseapp.com",
-  projectId: "typing-app-566de",
-  storageBucket: "typing-app-566de.firebasestorage.app",
-  messagingSenderId: "650860083709",
-  appId: "1:650860083709:web:d6d0f0a0138dc7548f0194"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
